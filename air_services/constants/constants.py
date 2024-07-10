63c9eea981a417f2ab6dd4d7b1f45164e6d6c2b7
root <km@larriveefontaine.ca>
2024-07-10 00:18:32 +0000

    trying to create a first test

---
+++ air_services/constants/constants.py
@@ -0,0 +1,4 @@
+import math as _math
+
+# mathematical constants
+pi = _math.pi
---
+++ tests/test_geometry.py
@@ -0,0 +1,4 @@
+from air_services.geometry import Elbow_centerline_length
+
+print(Elbow_centerline_length(4, 2.5, 2, 2, angle=90))
+
